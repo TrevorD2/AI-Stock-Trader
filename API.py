979de2a57c05e6c0e57b64946c75da01c39d56bf
Trevor Douglas <145886230+TrevorD2@users.noreply.github.com>
2025-01-13 21:59:16 +0000

    Added template get request

--- API.py
+++ API.py
@@ -1,3 +1,13 @@
 import requests
 
-print("testing")
+headers = {
+    'Content-Type': 'application/json'
+}
+
+requestResponse = requests.get("https://api.tiingo.com/tiingo/daily/aapl/prices?startDate=2019-01-02&token=acf127c746b3b029d5c57d622cf64f85aed047fe", headers=headers)
+
+def write(text):
+    with open("out.txt", "w") as f:
+        f.write(str(text))
+
+write(requestResponse.json())
