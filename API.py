92bc8110ca4a78ae45527bd5f17886f894774e2e
Trevor Douglas <145886230+TrevorD2@users.noreply.github.com>
2025-02-25 22:08:14 -0800

    Added pandas df + logged / normalized data

--- API.py
+++ API.py
@@ -1,4 +1,7 @@
 import requests
+import json as js
+import pandas as pd
+import math
 
 def write(text, file="out", extension=".txt"):
     with open(file+extension, "w") as f:
@@ -14,8 +17,7 @@ def get_historic_ticker(ticker: str, start_date: str, end_date: str) -> int:
     if type(json) != list: return 1
     elif len(json) == 0: return 1
 
-    json = {json[i]["date"][:10]: dict({key: json[i][key] for key in list(json[i].keys())[1:]}) for i in range(len(json))} 
-    
+    json = [{(ticker+"_"+key if key!="date" else key):(json[i][key][:10] if key == "date" else float(json[i][key])) for key in list(json[i].keys())} for i in range(len(json))] 
     return json
 
 def get_ticker(ticker: str) -> dict:
@@ -38,23 +40,39 @@ def get_ticker(ticker: str, date: str):
     if type(json) != list: return -1
     elif len(json) == 0: return -1
 
-    #print(f"JSON for {ticker}: ", json)
     return json[0] #Get last day value
 
 def compile_dataset(stocks: list[str], start_date, end_date):
-    data = dict()
+    data = []
 
     for stock in stocks:
-        data[stock]= get_historic_ticker(stock, start_date, end_date)
+        data.extend(get_historic_ticker(stock, start_date, end_date))
 
-    write("stock_data = " + str(data), "stock_data", ".py")
+    df = pd.json_normalize(data)
+    return df
+
+def read_data():
+    with open("stock_data.json", "r") as f:
+        json = js.load(f)
+        df = pd.json_normalize(json)
+    return df
+
+def log_data():
+    df = read_data()
+    df = df.map(lambda x: math.log(x)/10 if (type(x)==float and x > 0) else x) #Take the log of data, divide by 10 to get data to be roughly between 0 - 10
+    df.to_json("stock_data.json", orient="records")
+    return df
 
 if __name__ == "__main__":
-    
+    """
     stocks = [
         "AMZN",
-        "WMT",
-        "COST"
     ]
 
-    compile_dataset(stocks, "2000-01-01", "2025-01-01")
+    df = compile_dataset(stocks, "2000-01-01", "2025-01-01")
+    df.to_json("stock_data.json", orient="records")
+    
+    df = log_data()
+    
+    print(df.head(10))
+    """
